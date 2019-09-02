af8e6b6e85aed2e728324e55e4d9a103172d7620
Volodymyr Shvaiko <cub4lt@Mgmail.com>
2019-09-02 10:21:19 +0300

    rotate by day

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,7 +5,6 @@ chmod 0400 /etc/passwd-s3fs
 s3fs $S3_BUCKET $MNT_POINT -f -o url=${S3_ENDPOINT},allow_other,use_cache=/tmp,max_stat_cache_size=1000,stat_cache_expire=900,retries=5,connect_timeout=10${S3_EXTRAVARS} &
 sleep 5
 ls -al ${MNT_POINT} | wc -l
-#find ${MNT_POINT} -type f -mtime ${ROTATION_DAY_PERIOD} -exec rm -f {} \;
-find ${MNT_POINT} -type f -mmin ${ROTATION_MIN_PERIOD} -exec rm -f {} \;
+find ${MNT_POINT} -type f -mtime ${ROTATION_DAY_PERIOD} -exec rm -f {} \;
 ls -al ${MNT_POINT} | wc -l
 umount ${MNT_POINT}
