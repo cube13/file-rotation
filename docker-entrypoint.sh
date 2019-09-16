acbe018ff2de6efd6ac43db89250994d3fc12f09
Volodymyr Shvaiko <cub4lt@Mgmail.com>
2019-09-16 14:38:05 +0300

    fix count exec time

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,17 +2,19 @@
 #set -x
 echo "${AWS_ACCESS_KEY_ID}:${AWS_SECRET_ACCESS_KEY}" > /etc/passwd-s3fs  
 chmod 0400 /etc/passwd-s3fs
+echo "Mount bucket $S3_BUCKET to dir $MNT_POINT"
 s3fs $S3_BUCKET $MNT_POINT -f -o url=${S3_ENDPOINT},allow_other,use_cache=/tmp,max_stat_cache_size=1000,stat_cache_expire=900,retries=5,connect_timeout=10${S3_EXTRAVARS} &
 sleep 5
-COUNT_BEFORE=$(find ${MNT_POINT} -type f)
+echo "bucket $S3_BUCKET mounted to dir $MNT_POINT"
+COUNT_BEFORE=$(find ${MNT_POINT} -type f | wc -l)
 SIZE_BEFORE=$(du ${MNT_POINT} -hs)
 echo "$COUNT_BEFORE files with sum size $SIZE_BEFORE before rotatation"
 START=$(date +%s) 
 find ${MNT_POINT} -type f -mtime ${ROTATION_DAY_PERIOD} -exec rm -f {} \;
 END=$(date +%s)
-ROTATION_TIME=$(($end - $start))
+ROTATION_TIME=$((END - START))
 
-COUNT_AFTER=$(find ${MNT_POINT} -type f)
+COUNT_AFTER=$(find ${MNT_POINT} -type f | wc -l)
 SIZE_AFTER=$(du ${MNT_POINT} -hs)
 echo "Execution time: $ROTATION_TIME sec"
 echo "$COUNT_AFTER files with sum size $SIZE_AFTER after rotatation" 
